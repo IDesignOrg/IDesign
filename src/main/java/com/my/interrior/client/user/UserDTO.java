4f68efba977b7c451190860b2a169b1cb3530cb6
Park94chan <qcd8989@gmail.com>
2024-06-05 16:05:02 +0900

    DTO

--- src/main/java/com/my/interrior/client/user/UserDTO.java
+++ src/main/java/com/my/interrior/client/user/UserDTO.java
@@ -1,5 +1,17 @@
 package com.my.interrior.client.user;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@ToString
 public class UserDTO {
+
+    private String UId;
+
+    private String UPw;
+
+
 }
