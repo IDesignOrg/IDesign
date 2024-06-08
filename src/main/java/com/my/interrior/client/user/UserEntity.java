2609031526d430b22df15f50e0dd02d1ceb28d9e
sangju Han <sangju0501@gmail.com>
2024-06-08 14:38:13 +0900

    마이페이지 수정 완료

--- src/main/java/com/my/interrior/client/user/UserEntity.java
+++ src/main/java/com/my/interrior/client/user/UserEntity.java
@@ -1,12 +1,19 @@
 package com.my.interrior.client.user;
 
-import jakarta.persistence.*;
+import java.time.LocalDate;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.springframework.format.annotation.DateTimeFormat;
-
-import java.time.LocalDate;
 
 
 @Entity(name = "user")
@@ -17,27 +24,35 @@ public class UserEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonProperty("UNo")
     @Column(name = "u_no")
     private Long UNo;
 
+    @JsonProperty("UId")
     @Column(nullable = false, unique = true, name = "u_id")
     private String UId;
 
+    @JsonProperty("UPw")
     @Column(nullable = false, name = "u_pw")
     private String UPw;
 
+    @JsonProperty("UName")
     @Column(nullable = false, name = "u_name")
     private String UName;
 
+    @JsonProperty("UMail")
     @Column(nullable = false, name = "u_mail")
     private String UMail;
 
+    @JsonProperty("UBirth")
     @Column(nullable = false, name = "u_birth")
     private String UBirth;
 
+    @JsonProperty("UTel")
     @Column(nullable = false, name = "u_tel")
     private String UTel;
 
+    @JsonProperty("URegister")
     @Column(nullable = false, name = "u_register")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate URegister;
