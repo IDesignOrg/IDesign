f23f0d7bfe3eaecdaf266f2faedfc48c174a9079
sangju Han <sangju0501@gmail.com>
2024-06-08 14:38:13 +0900

    마이페이지 수정 완료

--- src/main/java/com/my/interrior/client/user/UserController.java
+++ src/main/java/com/my/interrior/client/user/UserController.java
@@ -1,18 +1,23 @@
 package com.my.interrior.client.user;
 
-import jakarta.servlet.http.HttpSession;
+import java.time.LocalDate;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.time.LocalDate;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class UserController {
 
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
     @Autowired
     private UserRepository userRepository; // JpaRepository(save, deleteBy)
 
@@ -30,27 +35,46 @@ public class UserController {
     }
 
     @PostMapping("/auth/join")
-    public String join(@ModelAttribute UserEntity userEntity){
-        userEntity.setURegister(LocalDate.now());
-        //insert
-        userRepository.save(userEntity);
+    public String join(@ModelAttribute UserEntity userEntity) throws Exception {
+    	 try {
+             userEntity.setURegister(LocalDate.now());
+             userEntity.setUPw(passwordEncoder.encode(userEntity.getUPw()));
+             if (userEntity.getURegister() == null || userEntity.getUPw() == null)
+                 return null;
+             //insert
+             userRepository.save(userEntity);
 
-        return "redirect:/auth/login"; // auth/login 경로로 리다이렉트
+             return "redirect:/auth/login"; // auth/login 경로로 리다이렉트
+         } catch (Exception e) {
+             e.printStackTrace();
+             return "error";
+         }
     }
 
     @PostMapping("/auth/login")
-    public String login(@ModelAttribute UserDTO userDTO, HttpSession session , Model model){
-        String UId = userDTO.getUId();
-        String UPw = userDTO.getUPw();
-        UserEntity userEntity = userService.checkLogin(UId, UPw);
-
-        if (userEntity != null) {
-            session.setAttribute("UId", userEntity.getUId());
-            return "redirect:/";
-        } else {
-            model.addAttribute("loginError", "입력하신 정보를 확인하세요.");
-            return "client/login";
-        }
+    public String login(@ModelAttribute UserDTO userDTO, HttpSession session , Model model) throws Exception{
+    	try {
+    		if(userDTO.getUId() == null || userDTO.getUPw() == null)
+                return "redirect:/auth/login";
+    		
+    		
+    		String UId = userDTO.getUId();
+    		String UPw = userDTO.getUPw();
+            UserEntity user = userService.checkLogin(UId);
+            System.out.println("user" + user);
+            if(user != null && passwordEncoder.matches(UPw, user.getUPw())) {
+                session.setAttribute("UId", user.getUId());
+                return "redirect:/";
+            } else {
+                model.addAttribute("loginError", "입력하신 정보를 확인하세요.");
+                return "client/login";
+            }
+    	} catch(Exception e) {
+    		e.printStackTrace();
+    		return "error";
+    	}
+    	
     }
+    
 }
 
