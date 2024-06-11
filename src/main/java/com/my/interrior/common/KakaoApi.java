303955e899e415455d59685cb085658fcaa600fc
wookie3739 <dnlsemtmf@gmail.com>
2024-06-11 14:07:18 +0900

    카카오로그인 끝, 구글 로그인 로직 진행중

---
+++ src/main/java/com/my/interrior/common/KakaoApi.java
@@ -0,0 +1,21 @@
+package com.my.interrior.common;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@Setter
+@Component
+public class KakaoApi {
+
+	@Value("${kakao.api_key}")
+	private String kakaoApiKey;
+	
+	@Value("${kakao.redirect_uri}")
+	private String kakaoRedirectUri;
+}
