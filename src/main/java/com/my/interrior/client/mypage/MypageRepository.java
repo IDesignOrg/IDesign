b40bc8bc58c499e07527a5c32666423c05015ac1
Park94chan <qcd8989@gmail.com>
2024-06-11 17:43:38 +0900

    마이페이지 유저 정보 삭제 추가기능 구현

--- src/main/java/com/my/interrior/client/mypage/MypageRepository.java
+++ src/main/java/com/my/interrior/client/mypage/MypageRepository.java
@@ -1,11 +1,18 @@
 package com.my.interrior.client.mypage;
 
-import com.my.interrior.client.user.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import com.my.interrior.client.user.UserEntity;
+
+import jakarta.transaction.Transactional;
+
+
 
 @Repository
 public interface MypageRepository extends JpaRepository<UserEntity, Long> {
 
     UserEntity findByUId(String UId);
+    @Transactional
+    void deleteByUId(String UId);
 }
