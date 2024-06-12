e4d20386d4caf6e3cdc8b5ddfa4d07b01c87da6a
unknown <dnlsemtmf@gmail.com>
2024-06-12 19:26:51 +0900

    비밀번호 찾기 완료

---
+++ src/main/java/com/my/interrior/client/user/UserMailDTO.java
@@ -0,0 +1,12 @@
+package com.my.interrior.client.user;
+
+import lombok.Data;
+
+@Data
+public class UserMailDTO {
+
+	private String UName;
+	
+	private String UMail;
+	
+}
--- src/main/java/com/my/interrior/client/user/UserRestController.java
+++ src/main/java/com/my/interrior/client/user/UserRestController.java
@@ -1,10 +1,40 @@
 package com.my.interrior.client.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.my.interrior.common.MailDTO;
+
+import jakarta.transaction.Transactional;
 
 
 @RestController
 public class UserRestController {
-	
 
+	@Autowired
+	private UserService userService;
+	
+	@Transactional
+	@GetMapping("/auth/findPw/{UName}")
+	public ResponseEntity<String> findUPw(@ModelAttribute UserMailDTO dto, @PathVariable("UName") String UName, Model model){
+		String mail = dto.getUMail();
+		String name = dto.getUName();
+		
+		MailDTO user = userService.checkMailAndName(mail, name);
+		
+		if(user != null) {
+			
+			userService.mailSend(user);
+			return ResponseEntity.ok("success");
+		}else {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("fail");
+		}
+		
+	}
 }
