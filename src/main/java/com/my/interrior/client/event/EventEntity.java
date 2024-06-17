597fff8a035627a786a9c0c0675ae62278697fdd
Park94chan <qcd8989@gmail.com>
2024-06-17 17:30:03 +0900

    이벤트페이지 및 기능 구현 DAY-1

--- src/main/java/com/my/interrior/client/event/EventEntity.java
+++ src/main/java/com/my/interrior/client/event/EventEntity.java
@@ -1,20 +1,30 @@
 package com.my.interrior.client.event;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@ToString
 @Entity(name = "event")
 public class EventEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "event_no")
-    private Long EventNo;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "event_no")
+	private Long eventNo;
 
-    @Column(nullable = false, name = "event_title")
-    private String EventTitle;
+	@Column(nullable = false, name = "event_title")
+	private String eventTitle;
 
-    @Column(nullable = false, columnDefinition = "TEXT", name = "event_content")
-    private String EventContent;
+	@Column(nullable = false, columnDefinition = "TEXT", name = "event_content")
+	private String eventContent;
 
-    @Column(nullable = false, name = "event_img")
-    private String EventImg;
+	@Column(nullable = false, name = "event_img")
+	private String eventImg;
 }
