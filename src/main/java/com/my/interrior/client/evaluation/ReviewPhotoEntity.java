e8a801113b8df399566d128008dc9a5e4dfbe4d6
DaRamGGi <jelly0379@gmail.com>
2024-06-17 22:20:59 +0900

    GCS를 이용해 사진 올리기까지는 성공

---
+++ src/main/java/com/my/interrior/client/evaluation/ReviewPhotoEntity.java
@@ -0,0 +1,29 @@
+package com.my.interrior.client.evaluation;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity (name = "review_photo")
+public class ReviewPhotoEntity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@JsonProperty("RpNo")
+	@Column(name = "rp_no")
+	private Long RpNo;
+	
+	@JsonProperty("RpPhoto")
+	@Column(name = "rp_photo")
+	private String RpPhoto;
+	
+	@ManyToOne
+	@JoinColumn(name = "r_no", referencedColumnName = "r_no")
+	private ReviewEntity review;
+}
