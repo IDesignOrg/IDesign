be76a133e17e405c9b84984d47224e16fc343928
JAEHWAN YOON <jelly0379@gmail.com>
2024-07-10 20:50:40 +0900

    마이페이지 수정 진행중

--- src/main/java/com/my/interrior/client/mypage/MypageController.java
+++ src/main/java/com/my/interrior/client/mypage/MypageController.java
@@ -3,10 +3,19 @@ package com.my.interrior.client.mypage;
 import com.my.interrior.client.user.UserEntity;
 import com.my.interrior.client.user.UserRepository;
 import jakarta.servlet.http.HttpSession;
+
+import java.lang.ProcessBuilder.Redirect;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 
 @Controller
 public class MypageController {
@@ -16,9 +25,12 @@ public class MypageController {
 
     @Autowired
     private MypageService mypageService;
+    
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
 
-    @GetMapping("/mypage")
+    @GetMapping("/mypage/mypage")
     public String myPage(HttpSession session, Model model) {
 
         String  UId = (String) session.getAttribute("UId");
@@ -28,8 +40,42 @@ public class MypageController {
         model.addAttribute("userInfo",user);
 
 
-        return "client/mypage";
+        return "client/mypage/mypage";
     }
+    @GetMapping("mypage/check_password")
+    public String getMethodName() {
+        return "client/mypage/check_password";
+    }
+    @PostMapping("mypage/check_password")
+    public String check_password(@RequestParam("password") String password, HttpSession session, RedirectAttributes redirectAttributes) {
+    	String UId = (String) session.getAttribute("UId");
+        UserEntity user = mypageService.getUserInfo(UId);
+
+        if (user != null && passwordEncoder.matches(password, user.getUPw())) {
+            return "redirect:/mypage/mypageUpdate";
+        } else {
+            redirectAttributes.addFlashAttribute("error", "Invalid password");
+            return "redirect:/mypage/check-password";
+        }
+    }
+    @GetMapping("mypage/mypageUpdate")
+    public String mypageUpdateG(HttpSession session, Model model) {
+    	String  UId = (String) session.getAttribute("UId");
+    	UserEntity user = mypageService.getUserInfo(UId);
+    	model.addAttribute("userInfo",user);
+    	return "client/mypage/mypageUpdate";
+    }
+    @PostMapping("mypage/mypageUpdate")
+    public String mypageUpdate( @RequestParam("file") MultipartFile file,
+            @RequestParam("UName") String UName,
+            @RequestParam("UBirth") String UBirth,
+            @RequestParam("UMail") String UMail,
+            @RequestParam("UTel") String UTel) {
+    	
+    	
+    	return "client/mypage/mypage";
+    }
+    
     
     
     
