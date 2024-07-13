241121e6657ce42112f2774bdb75abb6a4e758e5
JAEHWAN YOON <jelly0379@gmail.com>
2024-07-13 16:24:58 +0900

    샵테이블 만듬

---
+++ src/main/java/com/my/interrior/client/shop/ShopPhotoEntity.java
@@ -0,0 +1,26 @@
+package com.my.interrior.client.shop;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity(name="shop_photo")
+public class ShopPhotoEntity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long shopPhotoNo;
+	
+	@Column(name = "shop_photo_url")
+	private String shopPhotoUrl;
+	
+	@ManyToOne
+	@JoinColumn(name = "shop_no")
+	private ShopEntity shopEntity;
+	
+	
+}
