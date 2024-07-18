4aef5ff3299d57c5c5e1aa071a6e9e14702246fa
JAEHWAN YOON <jelly0379@gmail.com>
2024-07-18 17:32:59 +0900

    작성중

---
+++ src/main/java/com/my/interrior/client/shop/ShopOptionRepository.java
@@ -0,0 +1,7 @@
+package com.my.interrior.client.shop;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShopOptionRepository extends JpaRepository<ShopOptionEntity, Long>{
+
+}
