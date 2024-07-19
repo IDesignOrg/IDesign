f6a8e7eccc6b980ecc426617889a397d9a9a7667
JAEHWAN YOON <jelly0379@gmail.com>
2024-07-19 13:48:49 +0900

    shop작성페이지, shop리스트 만듬 디자인 하나도 안한 버전

--- src/main/java/com/my/interrior/client/shop/ShopPhotoEntity.java
+++ src/main/java/com/my/interrior/client/shop/ShopPhotoEntity.java
@@ -7,8 +7,12 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity(name="shop_photo")
+@Getter
+@Setter
 public class ShopPhotoEntity {
 	
 	@Id
