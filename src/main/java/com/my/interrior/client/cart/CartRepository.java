961e88b733b76bcd3d0c4fbc415a846a3ae3ad52
DaRamGGi <jelly0379@gmail.com>
2024-07-22 17:44:21 +0900

    상세페이지 + 장바구니 가는거까지

---
+++ src/main/java/com/my/interrior/client/cart/CartRepository.java
@@ -0,0 +1,7 @@
+package com.my.interrior.client.cart;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartRepository extends JpaRepository<CartEntity, Long>{
+
+}
