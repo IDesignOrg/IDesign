6d991c1cef4084f83c2131c0a884d8e1bccaff51
DaRamGGi <jelly0379@gmail.com>
2024-07-22 17:44:21 +0900

    상세페이지 + 장바구니 가는거까지

--- src/main/java/com/my/interrior/client/shop/ShopOptionValueRepository.java
+++ src/main/java/com/my/interrior/client/shop/ShopOptionValueRepository.java
@@ -5,5 +5,5 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ShopOptionValueRepository extends JpaRepository<ShopOptionValueEntity, Long>{
-	List<String> findByShopOptionEntity_shopOptionNo(List<Long> shopOptionNo);
+
 }
