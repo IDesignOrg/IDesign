7dffe8d8426c59c2d4a32c8e3d9950dd2017ef34
DaRamGGi <jelly0379@gmail.com>
2024-07-26 12:23:58 +0900

    gcs삭제 클래스, cart컨트롤러 서비스 옴김

--- src/main/java/com/my/interrior/client/cart/CartController.java
+++ src/main/java/com/my/interrior/client/cart/CartController.java
@@ -1,11 +1,37 @@
 package com.my.interrior.client.cart;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.my.interrior.client.shop.ShopService;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class CartController {
 	
+	@Autowired
+	private CartService cartService;
 	
+	@PostMapping("/cart")
+    public String goCart(
+    		@RequestParam("shopNo") Long shopNo,
+    		@RequestParam("options") List<Long> optionValueIds,
+    		@RequestParam("quantity") int quantity, HttpSession session) {
+    	
+    	String userId = (String) session.getAttribute("UId");
+    	
+    	if(userId == null) {
+    		return "redirect:/auth/login";
+    	}
+    	
+    	cartService.inCart(optionValueIds, shopNo, quantity);
+
+    	return "client/shop/shopList";
+    }
 
 }
