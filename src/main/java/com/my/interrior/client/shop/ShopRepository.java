ee1d784e78c7fd7ac11c053e5c2b79ba98b00460
JAEHWAN YOON <jelly0379@gmail.com>
2024-07-31 19:50:43 +0900

    shop검색기능 구현

--- src/main/java/com/my/interrior/client/shop/ShopRepository.java
+++ src/main/java/com/my/interrior/client/shop/ShopRepository.java
@@ -5,9 +5,12 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.my.interrior.client.cart.CartEntity;
+
 
 @Repository
 public interface ShopRepository extends JpaRepository<ShopEntity, Long>{
 	List<ShopEntity> findByShopNoIn(List<Long> shopNos);
+	List<ShopEntity> findByShopTitleContainingAndShopCategoryContaining(
+	        String shopTitle, String shopCategory
+	    );
 }
