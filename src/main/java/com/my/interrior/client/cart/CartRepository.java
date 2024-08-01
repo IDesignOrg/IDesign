fc263f5a5b517091c7654f1a54381973e3789b44
wookie3739 <dnlsemtmf@gmail.com>
2024-08-01 15:08:08 +0900

    장바구니 개별 삭제 구현 완료

--- src/main/java/com/my/interrior/client/cart/CartRepository.java
+++ src/main/java/com/my/interrior/client/cart/CartRepository.java
@@ -10,4 +10,6 @@ public interface CartRepository extends JpaRepository<CartEntity, Long>{
 	int countByUserEntity_UId(String UId);
 	List<CartEntity> findByUserEntity_UNo(Long UNo);
 	void deleteAllByUserEntity_UNo(Long userNo);
+//	CartEntity findByCNo(Long CNo);
+	void deleteByCNo(Long CNo);
 }
