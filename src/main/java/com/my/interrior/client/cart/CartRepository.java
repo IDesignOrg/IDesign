1cdb0cffc872a7eef2290d13a2219eac55f18118
DaRamGGi <jelly0379@gmail.com>
2024-08-01 16:39:15 +0900

    수정

--- src/main/java/com/my/interrior/client/cart/CartRepository.java
+++ src/main/java/com/my/interrior/client/cart/CartRepository.java
@@ -10,9 +10,8 @@ public interface CartRepository extends JpaRepository<CartEntity, Long>{
 	int countByUserEntity_UId(String UId);
 	List<CartEntity> findByUserEntity_UNo(Long UNo);
 	void deleteAllByUserEntity_UNo(Long userNo);
-<<<<<<< HEAD
+
 //	CartEntity findByCNo(Long CNo);
 	void deleteByCNo(Long CNo);
-=======
->>>>>>> 07027e0a20e87bebb2286801847af93f4c26cd1c
+
 }
