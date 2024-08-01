093366f78f61de0690c22753f5a954541965c7d6
DaRamGGi <jelly0379@gmail.com>
2024-08-01 16:32:56 +0900

    작업물 옴김

--- src/main/java/com/my/interrior/client/shop/ShopController.java
+++ src/main/java/com/my/interrior/client/shop/ShopController.java
@@ -99,4 +99,10 @@ public class ShopController {
     	model.addAttribute("shopPhoto", shopPhotos);
     	return "client/shop/shopUpdate";
     }
+    @GetMapping("/auth/shopReview/{shopNo}")
+    public String shopReview(Model model, @PathVariable("shopNo") Long shopNo) {
+    	Optional<ShopEntity> shops = shopService.getShopById(shopNo);
+    	model.addAttribute("shops", shops.get());
+    	return "client/shop/shopReview";
+    }
 }
