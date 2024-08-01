f5132c678f2c5b14de632b5c850701784bfb7b39
DaRamGGi <jelly0379@gmail.com>
2024-08-01 16:32:56 +0900

    작업물 옴김

---
+++ src/main/java/com/my/interrior/client/shop/ShopReviewRepository.java
@@ -0,0 +1,7 @@
+package com.my.interrior.client.shop;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShopReviewRepository extends JpaRepository<ShopReviewEntity, Long>{
+
+}
