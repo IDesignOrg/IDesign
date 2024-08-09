51d52589eb81a0c3c8890bd57e20f05c810a11d8
wookie3739 <dnlsemtmf@gmail.com>
2024-08-09 13:18:51 +0900

    환불 완료, 사전 결제 검증 완료, 결제 완료

--- src/main/java/com/my/interrior/client/pay/PaymentAndUserRepository.java
+++ src/main/java/com/my/interrior/client/pay/PaymentAndUserRepository.java
@@ -5,5 +5,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PaymentAndUserRepository extends JpaRepository<PaymentAndUserEntity, Long>{
-
+	void deleteByPayEntity_PayNo(Long payNo);
 }
