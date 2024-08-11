ad398ea5c2575fddf8466d3a0833fdd3e727a6d1
junheeLee96 <dlwnsgml203@naver.com>
2024-08-11 20:52:15 +0900

    rollback == > reset --hard to walls commit

---
+++ src/main/java/com/my/interrior/admin/page/CouponDTO.java
@@ -0,0 +1,11 @@
+package com.my.interrior.admin.page;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CouponDTO {
+	private Long userNo;
+	private Long couponNo;
+}
