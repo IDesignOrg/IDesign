4a88cce8b9a523a687c2a0c32b314a5f9b5397a6
wookie3739 <dnlsemtmf@gmail.com>
2024-08-11 14:45:20 +0900

    주문 내역에 배송쪽 로직 만듦

--- src/main/java/com/my/interrior/IDesignApplication.java
+++ src/main/java/com/my/interrior/IDesignApplication.java
@@ -2,8 +2,10 @@ package com.my.interrior;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling // schedule 사용
 public class IDesignApplication {
 
     public static void main(String[] args){
