2716572775e0025b04e5dd74f6145160a9bf3c4d
wookie3739 <dnlsemtmf@gmail.com>
2024-08-21 23:27:50 +0900

    test0821

--- src/main/java/com/my/interrior/three/ThreeRepository.java
+++ src/main/java/com/my/interrior/three/ThreeRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ThreeRepository extends JpaRepository<ThreeEntity, Long> {
+public interface ThreeRepository extends JpaRepository<ThreeEntity, String> {
 
 }
