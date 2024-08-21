148a59cfc0abfb643f70bfca8eb492f456782d20
DaRamGGi <jelly0379@gmail.com>
2024-08-21 14:47:51 +0900

    401에러페이지

---
+++ src/main/java/com/my/interrior/error/CustomErrorController.java
@@ -0,0 +1,19 @@
+package com.my.interrior.error;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@Controller
+public class CustomErrorController {
+
+	@GetMapping("/error/401")
+	@ResponseStatus(HttpStatus.UNAUTHORIZED)
+	public String unauthorized() {
+		return "error/401";
+	}
+}
