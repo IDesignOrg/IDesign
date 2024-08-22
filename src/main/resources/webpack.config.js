07d80d11d781dede32e3d7158c7de8981dc45991
junheeLee96 <dlwnsgml203@naver.com>
2024-08-22 16:41:40 +0900

    webpack config 수정

--- src/main/resources/webpack.config.js
+++ src/main/resources/webpack.config.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c73d077bccee2d66ed3339a5b4edbdb37632ec9152a3e3d9ad9c349ca746b0c7
-size 3766
+oid sha256:312e5e5600d958ba11577891e5038ada13728762119a1ab76c714e80b6bd8821
+size 3739
