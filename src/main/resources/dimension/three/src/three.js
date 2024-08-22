11d9051a448091485e82fb08636bd2430bd958f8
junheeLee96 <dlwnsgml203@naver.com>
2024-08-22 15:42:35 +0900

    window. resize

--- src/main/resources/dimension/three/src/three.js
+++ src/main/resources/dimension/three/src/three.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b2fbe15183044bc861183d1af8403ca755516e66efc05324dc9f89e5126b1eac
-size 25992
+oid sha256:0c7c8a4393de45e4f12e1d31c0e47f499cc7f7804fd128b97352bf7da8e62bf5
+size 26196
