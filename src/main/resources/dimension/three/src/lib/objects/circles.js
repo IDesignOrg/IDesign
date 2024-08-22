7386e044e16d4f0f63551364799505b205e13ba7
junheeLee96 <dlwnsgml203@naver.com>
2024-08-22 17:33:59 +0900

    save data

--- src/main/resources/dimension/three/src/lib/objects/circles.js
+++ src/main/resources/dimension/three/src/lib/objects/circles.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f8d6935711374dc04dc13b89c34f191e862949e0541b938bccf146419155be0f
-size 822
+oid sha256:afe4f95cccda9cb50bc481b06f0bb3c0f2d4ec11ee77c53836615149a6cc7b20
+size 1019
