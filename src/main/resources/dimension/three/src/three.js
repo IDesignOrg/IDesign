2b38b2f6f54c1745a15794fba3ba35400b8af813
junheeLee96 <dlwnsgml203@naver.com>
2024-08-22 16:38:52 +0900

    mouse event change to canvas

--- src/main/resources/dimension/three/src/three.js
+++ src/main/resources/dimension/three/src/three.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7da6f25f42e359be766e7a9468c385a63b7953bde7771d3a795266c368368055
-size 26155
+oid sha256:45e933b6905b551e6fafa657d9adec7be4100ccacc01ec6db87bacd85090c893
+size 25602
