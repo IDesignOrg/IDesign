3f2d8db8128546211806acc5ba75b7d9429549fe
junheeLee96 <dlwnsgml203@naver.com>
2024-08-26 16:44:34 +0900

    shadow error fix

--- src/main/resources/dimension/three/src/lib/objects/Arrow.js
+++ src/main/resources/dimension/three/src/lib/objects/Arrow.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:75b0fe11fae84539a08ac365866d17d2fbda76e46fc12980b53aca2b2953eb7f
-size 1912
+oid sha256:cdf94faae90f43fe8ad5c21a54a95427641267318828bd151635b85bb8f6b51a
+size 1822
