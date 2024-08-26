4c6ce326279c1ded4047fe04ee801fd318607e04
wookie3739 <dnlsemtmf@gmail.com>
2024-08-26 17:15:09 +0900

    test123

--- src/main/resources/dimension/three/src/saveFactory.js
+++ src/main/resources/dimension/three/src/saveFactory.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9891f41b663b3e4097f466ece64a0f84c0997905c8ae2685527b57f900cd4a69
-size 1819
+oid sha256:4439235b312cf49e6cdaece5235a0de76252b321e614590ae72f501f232bbbaf
+size 1680
