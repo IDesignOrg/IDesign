dd7c9955f96f425934a9b6b074b3c09584329f75
wookie3739 <dnlsemtmf@gmail.com>
2024-08-26 17:15:09 +0900

    test123

--- src/main/java/com/my/interrior/three/ChildRelationshipEntity.java
+++ src/main/java/com/my/interrior/three/ChildRelationshipEntity.java
@@ -1,6 +1,7 @@
 package com.my.interrior.three;
 
-import jakarta.persistence.Column;
+import com.my.interrior.three.SaveProjectRequest.DataRequest;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -13,26 +14,22 @@ import lombok.Setter;
 import lombok.ToString;
 
 @Entity
-@Table(name = "point")
+@Table(name = "child_relationship")
 @Getter
 @Setter
 @ToString
-public class PointEntity {
+public class ChildRelationshipEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@Column(name = "x")
-	private Double x;
-	
-	@Column(name = "y")
-	private Double y;
-	
-	@Column(name = "z")
-	private Double z;
-	
 	@ManyToOne
-	@JoinColumn(name = "oid", nullable = false)
-	private DataEntity data;
+	@JoinColumn(name = "parent_id")
+	private DataEntity parent;
+
+	@ManyToOne
+	@JoinColumn(name = "child_id")
+	private DataEntity child;
+	
 }
