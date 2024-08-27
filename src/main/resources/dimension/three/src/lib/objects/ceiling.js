537ba3daa6a32f0cca177385ef0f720c60fca19c
junheeLee96 <dlwnsgml203@naver.com>
2024-08-27 12:04:44 +0900

    ceiling paper

--- src/main/resources/dimension/three/src/lib/objects/ceiling.js
+++ src/main/resources/dimension/three/src/lib/objects/ceiling.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cb8b79408e9544dea9a8f1f71e8e62d795c90ddbcf54119dd6d2b9c04f6a45a6
-size 897
+oid sha256:a6de181b6fb224603254c31835e2872ebfb35ece1edf9240fa548b121d8f8da6
+size 884
