9550d6d87f1d236da396fd5f8c6a52557e90eb77
junheeLee96 <dlwnsgml203@naver.com>
2024-08-27 19:12:55 +0900

    furnitures

--- src/main/resources/dimension/three/src/three.js
+++ src/main/resources/dimension/three/src/three.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a4207da8e5306c502ead61d6e1887fc9d0355d975408d43db3e0b2fc2032dce3
-size 21313
+oid sha256:73e824810870cf4a1da87a9a4171b7542e8e6d5b8bfdde88550f64a976282f8d
+size 20381
