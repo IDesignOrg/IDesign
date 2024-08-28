2c4f530323cec53f64665421e58cfc8e227185f9
junheeLee96 <dlwnsgml203@naver.com>
2024-08-29 00:41:25 +0900

    remove lfs

--- src/main/resources/dimension/three/src/lib/loader/FontLoader/FontLoader.js
+++ src/main/resources/dimension/three/src/lib/loader/FontLoader/FontLoader.js
@@ -1,3 +1,28 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8a4034d08cb09bf2ce0017d98b6cf327eaf416203eb65f87c3592acddbd474f2
-size 721
+import { FontLoader } from "three/examples/jsm/loaders/FontLoader.js";
+import { TextGeometry } from "three/addons/geometries/TextGeometry.js";
+
+export let loadedFont = null;
+
+const loadFont = () => {
+  const loader = new FontLoader();
+  return new Promise((resolve, reject) => {
+    console.log("resolve!!");
+    loader.load(
+      "https://threejs.org/examples/fonts/helvetiker_regular.typeface.json",
+      (font) => {
+        loadedFont = font;
+        resolve(font);
+      },
+      undefined,
+      (err) => {
+        reject(err);
+      }
+    );
+  });
+};
+loadFont();
+
+// // 폰트를 로드합니다.
+// loadFont().then((font) => {
+//   // 모든 Arrow 인스턴스를 여기에서 생성할 수 있습니다.
+// });
--- src/main/resources/dimension/three/src/lib/loader/papers.js
+++ src/main/resources/dimension/three/src/lib/loader/papers.js
@@ -1,3 +1,22 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:01577884158106199ba52d6cf82407dbae74c1bf7a3959a8036c2eb7833ce03a
-size 710
+import { THREE } from "./three.js";
+
+const ceilingPaper = "/dimension/public/img/ceiling.jpg";
+const floorPaper = "/dimension/public/img/floor.jpg";
+const wallpaper = "/dimension/public/img/wallpaper.jpeg";
+
+const floorTexture = new THREE.TextureLoader().load(floorPaper);
+const wallTexture = new THREE.TextureLoader().load(wallpaper);
+const ceilingTexture = new THREE.TextureLoader().load(ceilingPaper);
+
+export const wallMaterial = new THREE.MeshBasicMaterial({
+  map: wallTexture,
+});
+export const ceilingMaterial = new THREE.MeshBasicMaterial({
+  map: ceilingTexture,
+  side: THREE.BackSide,
+});
+
+export const floorMaterial = new THREE.MeshBasicMaterial({
+  map: floorTexture,
+  side: THREE.FrontSide,
+});
