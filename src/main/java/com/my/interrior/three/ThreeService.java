5a626354058e414def9b4e25eda70c724f7c0d4c
wookie3739 <dnlsemtmf@gmail.com>
2024-08-30 19:33:22 +0900

    thumbnail에 GCS 필요

--- src/main/java/com/my/interrior/three/ThreeService.java
+++ src/main/java/com/my/interrior/three/ThreeService.java
@@ -5,6 +5,8 @@ import java.util.Map;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.my.interrior.client.user.UserEntity;
 import com.my.interrior.client.user.UserRepository;
@@ -17,55 +19,58 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class ThreeService {
 
-    private final UserRepository userRepository;
-    private final ThreeRepository threeRepository;
-    private final PointRepository pointRepository;
-    private final DataRepository dataRepository;
+	private final UserRepository userRepository;
+	private final ThreeRepository threeRepository;
+	private final PointRepository pointRepository;
+	private final DataRepository dataRepository;
 //    private final ChildRelationshipRepository childRelationshipRepository;
 
-    @Transactional
-    public void saveData(SaveProjectRequest request, String userId) {
-        UserEntity user = userRepository.findByUId(userId);
-        String projectId = request.getProjectId();
+	@Transactional
+	public void saveData(SaveProjectRequest request,
+		MultipartFile thumbnail, String userId) {
+		UserEntity user = userRepository.findByUId(userId);
+		String projectId = request.getProjectId();
 
-        log.info("userId : {}, projectId : {}, user : {}", userId, projectId, user);
+		log.info("userId : {}, projectId : {}, user : {}", userId, projectId, user);
+		log.info("thumbnail: {}, src: {}, title: {}", request.getThumbnail(), request.getProjectSrc().getSrc(), request.getProjectSrc().getTitle());
 
-        // 저장하기 전에 ThreeEntity 먼저 저장시켜야 함.
-        ThreeEntity threeEntity = new ThreeEntity();
-        threeEntity.setProjectId(projectId);
-        threeEntity.setUserEntity(user);
-        // three 저장
-        threeRepository.save(threeEntity);
+		// 저장하기 전에 ThreeEntity 먼저 저장시켜야 함.
+		ThreeEntity threeEntity = new ThreeEntity();
+		threeEntity.setProjectId(projectId);
+		threeEntity.setUserEntity(user);
+		threeEntity.setSrc(request.getProjectSrc().getSrc());
+		threeEntity.setTitle(request.getProjectSrc().getTitle());
+		// three 저장
+		threeRepository.save(threeEntity);
 
-        // data 저장
-        Map<String, DataEntity> dataMap = new HashMap<>();
+		// data 저장
+		Map<String, DataEntity> dataMap = new HashMap<>();
 
-        // Step 1: 모든 데이터 엔티티를 저장
-        for (SaveProjectRequest.DataRequest dataRequest : request.getDataEntities()) {
-        	log.info("dataRequest의 값들 : {}", dataRequest);
-            DataEntity data = new DataEntity();
-            data.setOid(dataRequest.getOid());
-            data.setType(dataRequest.getType());
-            data.setRotation(dataRequest.getRotation());
-            data.setParent(dataRequest.getParent());
-            data.setChildren(dataRequest.getChildren());
-            data.setThreeEntity(threeEntity);
+		// Step 1: 모든 데이터 엔티티를 저장
+		for (SaveProjectRequest.DataRequest dataRequest : request.getDataEntities()) {
+			log.info("dataRequest의 값들 : {}", dataRequest);
+			DataEntity data = new DataEntity();
+			data.setOid(dataRequest.getOid());
+			data.setType(dataRequest.getType());
+			data.setRotation(dataRequest.getRotation());
+			data.setParent(dataRequest.getParent());
+			data.setChildren(dataRequest.getChildren());
+			data.setThreeEntity(threeEntity);
 
-            data = dataRepository.save(data);
-            dataMap.put(data.getOid(), data);
+			data = dataRepository.save(data);
+			dataMap.put(data.getOid(), data);
 
-            if (dataRequest.getPoints() != null) {
-                for (SaveProjectRequest.PointRequest pointRequest : dataRequest.getPoints()) {
-                    PointEntity point = new PointEntity();
-                    point.setX(pointRequest.getX());
-                    point.setY(pointRequest.getY());
-                    point.setZ(pointRequest.getZ());
-                    point.setData(data);
+			if (dataRequest.getPoints() != null) {
+				for (SaveProjectRequest.PointRequest pointRequest : dataRequest.getPoints()) {
+					PointEntity point = new PointEntity();
+					point.setX(pointRequest.getX());
+					point.setY(pointRequest.getY());
+					point.setZ(pointRequest.getZ());
+					point.setData(data);
 
-                    pointRepository.save(point);
-                }
-            }
-        }
-    }
+					pointRepository.save(point);
+				}
+			}
+		}
+	}
 }
-
