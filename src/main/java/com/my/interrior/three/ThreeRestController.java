1a7197375cb3a9952b60593386f57063deded74c
unknown <dnlsemtmf@gmail.com>
2024-09-01 14:59:01 +0900

    임시

--- src/main/java/com/my/interrior/three/ThreeRestController.java
+++ src/main/java/com/my/interrior/three/ThreeRestController.java
@@ -2,8 +2,11 @@ package com.my.interrior.three;
 
 import java.io.IOException;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -25,23 +28,60 @@ public class ThreeRestController {
 	private final DataRepository dataRepository;
 	private final ThreeService threeService;
 	private final ChildRelationshipRepository childRelationshipRepository;
-	
-	
+
 	@PostMapping("/save/project")
 	public ResponseEntity<?> saveProject(@RequestPart("jsonData") SaveProjectRequest request,
-			@RequestPart("file") MultipartFile thumbnail,
-			HttpSession session) throws IOException{
+			@RequestPart("file") MultipartFile thumbnail, HttpSession session) throws IOException {
 		String userId = (String) session.getAttribute("UId");
-		
+
 		log.info("request DATA: {}, request FILE: {}", request, thumbnail);
-		
+
 		threeService.saveData(request, thumbnail, userId);
-		
+
 		return ResponseEntity.ok().build();
 	}
-	
-	
-	
+
+	// filter : 빈 문자열, sort : 문자열, flag : 정수형 Request
+	// response : status : success, fail || response: 200, errorcode || data:
+	// {projects:[], flag_num : 0 => 0~11, 1=> 12 ~23}
+	@GetMapping("/get/projects")
+	public ResponseEntity<?> getProjects(@RequestBody GetProjectsRequest request, HttpSession session)
+			throws IOException {
+
+		String userId = (String) session.getAttribute("UId");
+		String filter = request.getFilter();
+		String sort = request.getSort();
+		int flag = request.getFlag();
+		// 해당 사용자의 프로젝트 갯수 구하기(페이징 조건)
+		int count = threeService.getCounts(userId);
+		int dividedCount = (count / 12) - 1;
+		
+		log.info("userId: {}, filter: {}, sort:{}", userId, filter, sort);
+		// 해당 사용자의 프로젝트 구하기
+		// 사용자가 없으면(얼리 리턴 패턴)
+		if (userId == null) return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+		
+		
+		//임시 테스트
+		return ResponseEntity.ok().build();
+		// 만약 데이터베이스에 가지고있는 데이터가 없으면
+//		if (count == 0) {
+//			flag = -1;
+//			return ResponseEntity.ok(flag);
+//		} else {
+//			// 있으면 reg_dt 기준으로 최신 순 12개씩 가져오기. 최신순
+//			if(sort == "recent") {
+//				
+//				threeService.getTop12ProjectsByUserId(userId);
+//			}else if(sort == "asc") {
+//				threeService.getTop12ProjectsByUserIdOrderByTitleAsc(userId);
+//			}else {
+//				threeService.getTop12ProjectsByUserIdOrderByTitleDesc(userId);
+//			}
+//		}
+
+	}
+
 //	// 나중에 RestController로 옮길 예정
 //		@Transactional
 //		@PostMapping("/save/project")
@@ -105,6 +145,5 @@ public class ThreeRestController {
 //				saveDataEntity(childRequest, threeEntity, data);
 //			}
 //		}
-	
-	
+
 }
