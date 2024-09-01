926715e8c53a2f821e252a1b5e526b4aa6425971
JAEHWAN YOON <jelly0379@gmail.com>
2024-09-01 17:51:38 +0900

    dev와병합

--- src/main/java/com/my/interrior/three/ThreeRestController.java
+++ src/main/java/com/my/interrior/three/ThreeRestController.java
@@ -2,9 +2,11 @@ package com.my.interrior.three;
 
 import java.io.IOException;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -14,11 +16,10 @@ import com.my.interrior.client.user.UserRepository;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import lombok.extern.slf4j.Slf4j;
+
 
 @RestController
 @RequiredArgsConstructor
-@Slf4j
 @Slf4j
 public class ThreeRestController {
 
