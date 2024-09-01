f30d2c14fa063a981737e8b6ccc96d975ceb1e73
junheeLee96 <dlwnsgml203@naver.com>
2024-09-01 23:52:30 +0900

    yml file, 프로퍼티스 파일 ㅜ정

---
+++ init.sql
@@ -0,0 +1,5 @@
+-- Create the database if it does not exist
+CREATE DATABASE IF NOT EXISTS idesign;
+
+-- Optionally, you can switch to the database and create tables or other schema objects
+USE idesign;
