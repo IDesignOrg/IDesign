0b25bfb744b5b845a706bc6a1e69d0ef19236602
junheeLee96 <dlwnsgml203@naver.com>
2024-09-11 17:54:07 +0900

    webpack 수정햇음

--- src/main/resources/webpack.config.js
+++ src/main/resources/webpack.config.js
@@ -14,28 +14,17 @@ module.exports = {
     three: "./dimension/three/src/three.js",
   },
   output: {
-    publicPath: "/dist/",
     path: path.resolve("./dist"),
     filename: "[name].min.js",
+    publicPath: "/",
   },
   devServer: {
     port: 3000,
-    static: [
-      {
-        directory: path.join(__dirname),
-        publicPath: "/",
-        serveIndex: true,
-      },
-      // {
-      //   directory: path.join(__dirname, "three"),
-      //   publicPath: "/dimension/three/src/three.js",
-      // },
-      // {
-      //   directory: path.join(__dirname, "dashboard"),
-      //   publicPath: "/",
-      // },
-    ],
     liveReload: true,
+    static: {
+      directory: path.join(__dirname),
+    },
+    historyApiFallback: true, // For single-page applications
   },
   optimization: {
     minimizer:
@@ -56,10 +45,6 @@ module.exports = {
   },
   module: {
     rules: [
-      // {
-      //   test: /\.(png|jpe?g|gif|svg)$/i, // 이미지 파일 로더
-      //   type: "public/resource", // Webpack 5에서는 기본적으로 asset/resource를 사용하여 이미지를 처리
-      // },
       {
         test: /\.js$/,
         loader: "babel-loader",
@@ -70,18 +55,12 @@ module.exports = {
         enforce: "pre",
         use: ["source-map-loader"],
       },
-      {
-        test: /\.css$/,
-        include: path.resolve(__dirname, "dimension/public"), // 특정 폴더만 포함
-        use: ["style-loader", "css-loader"],
-      },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./dimension/three/three.html", // 상대 경로로 수정
       filename: "three.html",
-      chunks: ["three"],
+      template: "./dimension/three/three.html",
       minify:
         process.env.NODE_ENV === "production"
           ? {
@@ -91,9 +70,8 @@ module.exports = {
           : false,
     }),
     new HtmlWebpackPlugin({
-      template: "./dimension/dashboard/dashboard.html", // 상대 경로로 수정
       filename: "dashboard.html",
-      chunks: ["dashboard"],
+      template: "./dimension/dashboard/dashboard.html",
       minify:
         process.env.NODE_ENV === "production"
           ? {
@@ -102,22 +80,17 @@ module.exports = {
             }
           : false,
     }),
-
     new CleanWebpackPlugin(),
     // CopyWebpackPlugin: 그대로 복사할 파일들을 설정하는 플러그인
     // 아래 patterns에 설정한 파일/폴더는 빌드 시 dist 폴더에 자동으로 생성됩니다.
     // patterns에 설정한 경로에 해당 파일이 없으면 에러가 발생합니다.
     // 사용하는 파일이나 폴더 이름이 다르다면 변경해주세요.
     // 그대로 사용할 파일들이 없다면 CopyWebpackPlugin을 통째로 주석 처리 해주세요.
-    new CopyWebpackPlugin({
-      patterns: [
-        { from: "./dimension/public/reset.css", to: "public/reset.css" }, // 공용 CSS 파일은 별도로 복사
-        { from: "./dimension/public/three.css", to: "public/three.css" },
-        {
-          from: "./dimension/public/",
-          to: "public",
-        },
-      ],
-    }),
+    // new CopyWebpackPlugin({
+    //   patterns: [
+    //     { from: "./src/main.css", to: "./main.css" },
+    //     { from: "./src/images", to: "./images" },
+    //   ],
+    // }),
   ],
 };
