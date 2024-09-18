349425c1ccf1c17fdbd7e68f4f3cec69e4f0acaf
wookie3739 <dnlsemtmf@gmail.com>
2024-09-18 20:28:47 +0900

    대시보드 불러오기 api, 삭제 api 구현 중

---
+++ src/main/java/com/my/interrior/three/CreateProjectRequest.java
@@ -0,0 +1,14 @@
+package com.my.interrior.three;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class CreateProjectRequest {
+
+	private String src;
+	private String title;
+}
--- src/main/java/com/my/interrior/three/DataRepository.java
+++ src/main/java/com/my/interrior/three/DataRepository.java
@@ -1,7 +1,10 @@
 package com.my.interrior.three;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DataRepository extends JpaRepository<DataEntity, Long>{
 
+	List<DataEntity> findByThreeEntity(ThreeEntity threeEntity);
 }
