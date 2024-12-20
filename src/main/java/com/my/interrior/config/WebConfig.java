fdaf2eb9866f7dc63f5e37550d1f275b76e8e8aa
JAEHWAN YOON <jelly0379@gmail.com>
2024-12-20 16:33:49 +0900

    WebConfig에서 PATCH추가

--- src/main/java/com/my/interrior/config/WebConfig.java
+++ src/main/java/com/my/interrior/config/WebConfig.java
@@ -48,7 +48,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("https://idesign.r-e.kr")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
